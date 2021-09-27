ef150d3932a5b85b4e25b09762f78a73a84922aa
Priteshraj Bhangale <priteshraj.bhangale@morningstar.com>
2021-09-27 16:56:47 +0530

    [Pritesh] Displayed Greeting message

--- src/main/java/com/grretingappdevelopment/controller/GreetinfRestController.java
+++ src/main/java/com/grretingappdevelopment/controller/GreetinfRestController.java
@@ -1,7 +1,12 @@
 package com.grretingappdevelopment.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.grretingappdevelopment.service.GreetingService;
@@ -16,4 +21,20 @@ public class GreetinfRestController {
 	{
 		return gret.helloService();
 	}
+	
+	  
+    @GetMapping("/json")
+    public Map<String, String> jsonn() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("key", "value");
+        return map;
+    }
+    
+    @PostMapping("/postjson")
+    public Map<String, String> json() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("key", "value");
+        return map;
+    }
+
 }
