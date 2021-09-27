b60bbe8c278aedc9f01bb2965f39b5bd2bfeb497
Priteshraj Bhangale <priteshraj.bhangale@morningstar.com>
2021-09-27 16:50:33 +0530

    [Pritesh] Initial commit

---
+++ src/main/java/com/grretingappdevelopment/GrretingAppDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.grretingappdevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrretingAppDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrretingAppDevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grretingappdevelopment/GrretingAppDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grretingappdevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrretingAppDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
